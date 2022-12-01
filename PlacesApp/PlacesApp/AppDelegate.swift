a24fffa0f1b376274afb8d178104c86b2bfb4060
damiraliyev <damir.aliyev.02@mail.ru>
2022-12-01 10:56:12 +0600

    create the project and remove storyboard

---
+++ PlacesApp/PlacesApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlacesApp
+//
+//  Created by Damir Aliyev on 01.12.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+  
+
+}
+
---
+++ PlacesApp/PlacesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacesApp
+//
+//  Created by Damir Aliyev on 01.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+
+
+}
+
