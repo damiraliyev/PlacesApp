51950608b04a03c2fdb3f4e54ba69d33dae03c3f
damiraliyev <damir.aliyev.02@mail.ru>
2022-12-01 11:31:11 +0600

    add map to the view and add pinning functionality

--- PlacesApp/PlacesApp/ViewController.swift
+++ PlacesApp/PlacesApp/ViewController.swift
@@ -6,15 +6,83 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
+    
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .orange
+        setup()
+        layout()
+    }
+    
+    
+    func setup() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.delegate = self
+        setupTapGesture()
+    }
+    
+    func setupTapGesture() {
+        let longPressTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleTap))
+        mapView.addGestureRecognizer(longPressTapGesture)
+    }
+    
+    @objc func handleTap(gestureReconizer: UITapGestureRecognizer) {
+        let location = gestureReconizer.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        print("Pressed")
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+//        annotation.ima
+        mapView.addAnnotation(annotation)
+        
+    }
+    
+    
+    func layout() {
+        view.addSubview(mapView)
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
     }
 
 
 }
 
+extension ViewController: MKMapViewDelegate {
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        if annotation is MKUserLocation {
+//            return nil
+//        }
+//        
+//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+//        
+//        if annotationView == nil {
+//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+//        } else {
+//            annotationView?.annotation = annotation
+//        }
+////
+////        annotationView?.image = UIImage(systemName: "mappin")
+////        annotationView?.tintColor = .blue
+//    
+//        
+//        return annotationView
+//    }
+}
